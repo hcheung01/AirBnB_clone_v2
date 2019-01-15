b275b98f892a55488a99b2e89dfa2dbfee0ce563
Heindrick Cheung <hcheung01@gmail.com>
2019-01-15 08:02:37 +0000

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # setups web servers for deployment of web_static
+WHERE="/etc/nginx/sites-available/default"
 if ! dpkg -s nginx; then
     apt-get update
     if apt-get -y install nginx; then
-	WHERE="/etc/nginx/sites-available/default"
 	ADD="\\\tlocation \/redirect_me {\n\t\treturn 301 http://heindrickcheung.site;\n\t}\n"
 	E404="\\\n\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}\n"
 	apt-get update
@@ -14,12 +14,12 @@ if ! dpkg -s nginx; then
 	sed -i "48i $E404" $WHERE
     fi
 fi
-cp /etc/nginx/sites-available/default{,.orig}
+sudo cp /etc/nginx/sites-available/default{,.orig}
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "hello" | sudo tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-ADD2="\\\n\tlocation = /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
-sed -i "54i $ADD2" $WHERE
-service nginx restart
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo ADD2="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "54i $ADD2" $WHERE
+sudo service nginx restart
