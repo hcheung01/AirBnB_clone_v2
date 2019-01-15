db3723eeffdc36fa99bce1f859c1b6f4732fb806
Heindrick Cheung <hcheung01@gmail.com>
2019-01-15 22:08:21 +0000

    update

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+"""
+Frabic module
+"""
+from fabric.api import *
+from datetime import datetime
+from os import path
+
+
+def do_pack():
+    """pack directory tar format to another directory"""
+
+    local('mkdir -p versions')
+    format_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    filepath = 'versions/web_static_{}.tgz'.format(format_time)
+    cmd = "tar -cvzf {} web_static/".format(filepath)
+    try:
+        local(cmd, capture=True)
+        return filepath
+    except:
+        return
