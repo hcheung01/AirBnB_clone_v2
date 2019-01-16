5c39f843c3fbe63d652bd13679758c701793435d
Heindrick Cheung <hcheung01@gmail.com>
2019-01-16 00:43:05 +0000

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,15 +5,14 @@ if ! dpkg -s nginx; then
     apt-get update
     if apt-get -y install nginx; then
 	ADD="\\\tlocation \/redirect_me {\n\t\treturn 301 http://heindrickcheung.site;\n\t}\n"
-	E404="\\\n\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}\n"
-	HEADER="\\\n\tadd_header X-Served-By \$hostname;\n"
-	apt-get update
-	apt-get update && apt-get upgrade -y
+	HEADER="\\\n\tadd_header X-Served-By \$hostname;"
 	echo "Holberton School" | sudo tee /usr/share/nginx/html/index.html
 	sed -i "30i $ADD" $WHERE
-	echo -e "Ceci n'est pas une page\n" | sudo tee /usr/share/nginx/html/custom_404.html
-	sed -i "48i $E404" $WHERE
 	sed -i "29i $HEADER" $WHERE
+	new="\t\terror_page 404 @my404;"
+	sudo sed -i "/^\tlocation \/ {$/a \\$new" /etc/nginx/sites-available/default
+	new="location @my404 {\n\t\tdefault_type text/html;\n\t\treturn 404 \"Ceci n'est pas une page\";\n\t}"
+	sudo sed -i "/^\tserver_name localhost;$/a \\\n\\t$new" /etc/nginx/sites-available/default
     fi
 fi
 cp /etc/nginx/sites-available/default{,.orig}
@@ -22,6 +21,6 @@ mkdir -p /data/web_static/shared/
 echo "hello" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-ADD2="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
-sed -i "31i $ADD2" $WHERE
+ADD2="location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sed -i "/^\tserver_name localhost;$/a \\\n\\t$ADD2" $WHERE
 service nginx restart
