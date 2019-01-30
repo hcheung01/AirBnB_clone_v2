557ffd9e9f00937e1699619a88ac9f232efc5160
Heindrick Cheung <hcheung01@gmail.com>
2019-01-30 10:29:44 +0000

    removed port that was set to 5000

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,19 +3,22 @@
 Flask script that starts web application
 """
 from flask import Flask
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
     """Output Hello HBNB"""
-    return 'Hello HBNB!'
+    return ('Hello HBNB!')
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """Output HBNB"""
-    return 'HBNB'
+    return ('HBNB')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0')
