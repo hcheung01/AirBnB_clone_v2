c680228bf620693bcb8d58c123027199ade82bfc
Heindrick Cheung <hcheung01@gmail.com>
2019-01-31 00:20:42 +0000

    refactered

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,20 +4,23 @@ web application to generate list of states dynamically
 """
 from flask import Flask, render_template
 from models import storage
+from os import getenv
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states():
     """access File/DB Storage for all State objects and render to HTML"""
-    return render_template('7-states_list.html', storage=storage.all('State'))
+
+    obj = storage.all('State')
+    states = states = [v for k, v in obj.items()]
+    return render_template('7-states_list.html', storage=states)
 
 
 @app.teardown_appcontext
-def close(exception):
+def teardown_db(exception):
     """Closes the database again at the end of the request."""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
