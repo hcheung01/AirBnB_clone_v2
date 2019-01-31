b41d24f5cad193b416d399165daaf7755a8bc47b
Heindrick Cheung <hcheung01@gmail.com>
2019-01-31 01:54:22 +0000

    changed sql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,5 @@
--- creates the MySQL server test
-CREATE DATABASE
-IF NOT EXISTS hbnb_test_db;
-USE hbnb_test_db;
-CREATE USER
-IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
-USE performance_schema;
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+-- create database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
