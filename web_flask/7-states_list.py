a1dc881c5111c31451fdc2a625f555ccada3fb6a
Heindrick Cheung <hcheung01@gmail.com>
2019-02-01 08:32:35 +0000

    remove string enclosed value of port

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -22,6 +22,5 @@ def states():
     states = {v.id: v.name for k, v in obj.items()}
     return render_template('7-states_list.html', states=states)
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
